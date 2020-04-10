25770bed2d148fb54e16a1e8f4138f0dc3c4f25c
minhcongvghy <minhcongvghy@gmail.com>
2020-04-10 21:46:04 +0700

    export ra file excel từ DB của file upload

--- src/main/java/com/vsii/training/model/User.java
+++ src/main/java/com/vsii/training/model/User.java
@@ -3,15 +3,13 @@ package com.vsii.training.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
