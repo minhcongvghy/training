6525817227095256a7d3bd6795dbb17f4476de0e
minhcongvghy <minhcongvghy@gmail.com>
2020-04-13 15:30:58 +0700

    upload file lưu vào thư mục

--- src/main/java/com/vsii/training/TrainingApplication.java
+++ src/main/java/com/vsii/training/TrainingApplication.java
@@ -1,9 +1,14 @@
 package com.vsii.training;
 
+import com.vsii.training.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class TrainingApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/vsii/training/service/DBFileStorageService.java
+++ src/main/java/com/vsii/training/service/DBFileStorageService.java
@@ -1,18 +1,29 @@
 package com.vsii.training.service;
 
+import com.vsii.training.config.FileStorageProperties;
 import com.vsii.training.exception.FileStorageException;
 import com.vsii.training.exception.MyFileNotFoundException;
 import com.vsii.training.model.DBFile;
 import com.vsii.training.repository.DBFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 
 @Service
 public class DBFileStorageService {
+
+    private Path fileStorageLocation;
 
     @Autowired
     private DBFileRepository dbFileRepository;
@@ -20,13 +31,32 @@ public class DBFileStorageService {
     @Autowired
     private MailSenderService mailSenderService;
 
+    @Autowired
+    public void FileStorageService(FileStorageProperties fileStorageProperties) {
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
+                .toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
+
     public DBFile storeFile(MultipartFile file) {
         // Normalize file name
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
         // Check if the file's name contains invalid characters
-        if (fileName.contains("..")) {
-            throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
         }
 
         DBFile dbFile = new DBFile(fileName, file.getContentType(), file.getSize());
@@ -43,4 +73,5 @@ public class DBFileStorageService {
         return dbFileRepository.findById(fileId)
                 .orElseThrow(() -> new MyFileNotFoundException("File not found with id " + fileId));
     }
+
 }
